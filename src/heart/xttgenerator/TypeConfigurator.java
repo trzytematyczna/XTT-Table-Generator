5c89f3039995a51f053fffb731342e10a5413a3f
lucp <l.potepa@gmail.com>
2015-01-04 15:46:24 +0100

    really working ! :P

--- src/heart/xttgenerator/TypeConfigurator.java
+++ src/heart/xttgenerator/TypeConfigurator.java
@@ -29,6 +29,84 @@ public class TypeConfigurator {
 	private SetValue domain;
 	private SetValueConfigurator domainParam;
 	
+	public boolean validateConfiguration(){
+		if (this.length == null && this.lengthParam == null) return false;
+		if (this.length == null && this.lengthParam != null && (this.lengthParam.length != 2 || (this.lengthParam[0] >= this.lengthParam[1]) || this.lengthParam[0] < 0 || this.lengthParam[1] < 0)) return false;
+		if (this.base == null && this.baseParam == null) return false;
+		if (this.base != null && !(this.base.equals(Type.BASE_NUMERIC) || this.base.equals(Type.BASE_SYMBOLIC) || this.base.equals(Type.BASE_UNKNOWN))) return false;
+		if (this.ordered == null && this.orderedParam == null) return false;
+		if (this.ordered != null && !(this.ordered.equals(Type.ORDERED_YES) || this.ordered.equals(Type.ORDERED_NO) || this.ordered.equals(Type.ORDERED_UNKNOWN))) return false;
+		if (this.base != null && this.base.equals(Type.BASE_NUMERIC) && this.ordered != null && (this.ordered.equals(Type.ORDERED_NO) || this.ordered.equals(Type.ORDERED_UNKNOWN))) return false;
+		if (this.precision == null && this.precisionParam == null) return false;
+		if (this.precision == null && this.precisionParam != null && this.precisionParam < 0) return false;
+		if (this.domain == null && this.domainParam == null) return false;
+		if (this.domain == null && this.base != null && this.domainParam.validateConfiguration(this.base) == false) return false;
+		if (this.domain == null && this.baseParam == null && this.domainParam.validateConfiguration(Type.BASE_NUMERIC) == false) return false;
+		if (this.domain == null && this.baseParam == null && this.domainParam.validateConfiguration(Type.BASE_SYMBOLIC) == false) return false;
+		return true;
+	}
+	
+	public Type generateType(Random random) throws Exception{
+		Type type = null;
+		if (this.validateConfiguration()){
+			Type.Builder builder = new Type.Builder();
+			if (this.id != null) builder.setId(this.id);
+			else builder.setId(new String(GEN_ID + TYPE_COUNTER));
+			if (this.name != null) builder.setName(this.name);
+			else builder.setName(new String(GEN_NAME + TYPE_COUNTER));
+			if (this.length != null) builder.setLength(this.length);
+			else {
+				Integer pickedLength = random.nextInt(this.lengthParam[1] - this.lengthParam[0]) + this.lengthParam[0];
+				builder.setLength(pickedLength);
+			}
+			String pickedBase = this.base;
+			if (this.base != null) builder.setBase(this.base);
+			else {
+				Double shot = random.nextDouble();
+				if (this.baseParam[0] > shot) {
+					builder.setBase(Type.BASE_NUMERIC);
+					pickedBase = Type.BASE_NUMERIC;
+				}
+				else if (this.baseParam[0] + this.baseParam[1] > shot) {
+					builder.setBase(Type.BASE_SYMBOLIC);
+					pickedBase = Type.BASE_SYMBOLIC;
+				}
+				else {
+					builder.setBase(Type.BASE_UNKNOWN);
+					pickedBase = Type.BASE_UNKNOWN;
+				}
+			}
+			if (pickedBase.equals(Type.BASE_NUMERIC)) builder.setOrdered(Type.ORDERED_YES);
+			else{
+				if (this.ordered != null) builder.setOrdered(this.ordered);
+				else {
+					Double shot = random.nextDouble();
+					if (this.orderedParam[0] > shot) builder.setBase(Type.ORDERED_YES);
+					else if (this.orderedParam[0] + this.orderedParam[1] > shot) builder.setBase(Type.ORDERED_NO);
+					else builder.setBase(Type.ORDERED_UNKNOWN);
+				}
+			}
+			if (this.description != null) builder.setDescription(this.description);
+			else builder.setDescription(new String(GEN_DESC + TYPE_COUNTER));
+			if (this.precision != null) builder.setPrecision(this.precision);
+			else {
+				Integer pickedPrecision = random.nextInt(this.precisionParam);
+				builder.setPrecision(pickedPrecision);
+			}
+			if (this.domain != null) builder.setDomain(this.domain);
+			else{
+				builder.setDomain(this.domainParam.generateSetValue(random, pickedBase));
+			}
+			type = builder.build();
+		}
+		else{
+			throw new Exception(); //TODO exception
+		}
+		TypeConfigurator.TYPE_COUNTER++;
+		TypeConfigurator.types.add(type);
+		return type;
+	}
+	
 	public String getId() {
 		return id;
 	}
@@ -131,84 +209,6 @@ public class TypeConfigurator {
 
 	public void setDomainParam(SetValueConfigurator domainParam) {
 		this.domainParam = domainParam;
-	}
-	
-	public boolean validateConfiguration(){
-		if (this.length == null && this.lengthParam == null) return false;
-		if (this.length == null && this.lengthParam != null && (this.lengthParam.length != 2 || (this.lengthParam[0] >= this.lengthParam[1]) || this.lengthParam[0] < 0 || this.lengthParam[1] < 0)) return false;
-		if (this.base == null && this.baseParam == null) return false;
-		if (this.base != null && !(this.base.equals(Type.BASE_NUMERIC) || this.base.equals(Type.BASE_SYMBOLIC) || this.base.equals(Type.BASE_UNKNOWN))) return false;
-		if (this.ordered == null && this.orderedParam == null) return false;
-		if (this.ordered != null && !(this.ordered.equals(Type.ORDERED_YES) || this.ordered.equals(Type.ORDERED_NO) || this.ordered.equals(Type.ORDERED_UNKNOWN))) return false;
-		if (this.base != null && this.base.equals(Type.BASE_NUMERIC) && this.ordered != null && (this.ordered.equals(Type.ORDERED_NO) || this.ordered.equals(Type.ORDERED_UNKNOWN))) return false;
-		if (this.precision == null && this.precisionParam == null) return false;
-		if (this.precision == null && this.precisionParam != null && this.precisionParam < 0) return false;
-		if (this.domain == null && this.domainParam == null) return false;
-		if (this.domain == null && this.base != null && this.domainParam.validateConfiguration(this.base) == false) return false;
-		if (this.domain == null && this.baseParam == null && this.domainParam.validateConfiguration(Type.BASE_NUMERIC) == false) return false;
-		if (this.domain == null && this.baseParam == null && this.domainParam.validateConfiguration(Type.BASE_SYMBOLIC) == false) return false;
-		return true;
-	}
-	
-	public Type generateType(Random random) throws Exception{
-		Type type = null;
-		if (this.validateConfiguration()){
-			Type.Builder builder = new Type.Builder();
-			if (this.id != null) builder.setId(this.id);
-			else builder.setId(new String(GEN_ID + TYPE_COUNTER));
-			if (this.name != null) builder.setName(this.name);
-			else builder.setName(new String(GEN_NAME + TYPE_COUNTER));
-			if (this.length != null) builder.setLength(this.length);
-			else {
-				Integer pickedLength = random.nextInt(this.lengthParam[1] - this.lengthParam[0]) + this.lengthParam[0];
-				builder.setLength(pickedLength);
-			}
-			String pickedBase = this.base;
-			if (this.base != null) builder.setBase(this.base);
-			else {
-				Double shot = random.nextDouble();
-				if (this.baseParam[0] > shot) {
-					builder.setBase(Type.BASE_NUMERIC);
-					pickedBase = Type.BASE_NUMERIC;
-				}
-				else if (this.baseParam[0] + this.baseParam[1] > shot) {
-					builder.setBase(Type.BASE_SYMBOLIC);
-					pickedBase = Type.BASE_SYMBOLIC;
-				}
-				else {
-					builder.setBase(Type.BASE_UNKNOWN);
-					pickedBase = Type.BASE_UNKNOWN;
-				}
-			}
-			if (pickedBase.equals(Type.BASE_NUMERIC)) builder.setOrdered(Type.ORDERED_YES);
-			else{
-				if (this.ordered != null) builder.setOrdered(this.ordered);
-				else {
-					Double shot = random.nextDouble();
-					if (this.orderedParam[0] > shot) builder.setBase(Type.ORDERED_YES);
-					else if (this.orderedParam[0] + this.orderedParam[1] > shot) builder.setBase(Type.ORDERED_NO);
-					else builder.setBase(Type.ORDERED_UNKNOWN);
-				}
-			}
-			if (this.description != null) builder.setDescription(this.description);
-			else builder.setDescription(new String(GEN_DESC + TYPE_COUNTER));
-			if (this.precision != null) builder.setPrecision(this.precision);
-			else {
-				Integer pickedPrecision = random.nextInt(this.precisionParam);
-				builder.setPrecision(pickedPrecision);
-			}
-			if (this.domain != null) builder.setDomain(this.domain);
-			else{
-				builder.setDomain(this.domainParam.generateSetValue(random, pickedBase));
-			}
-			type = builder.build();
-		}
-		else{
-			throw new Exception(); //TODO exception
-		}
-		TypeConfigurator.TYPE_COUNTER++;
-		TypeConfigurator.types.add(type);
-		return type;
 	}
 	
 }
